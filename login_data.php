579db9afed71ca93827d853b0bd1b69a5ae54cab
peggycc <yellowflying210@gmail.com>
2015-11-23 14:22:44 +0800

    註冊Area
    註冊頁面修改

---
+++ login_data.php
@@ -0,0 +1,19 @@
+<?php
+	$link = mysql_connect("localhost","root","ytwu57874") or exit( mysql_error() );
+	mysql_select_db("user_data",$link) or die("db連線失敗");
+	mysql_query("set names utf8");
+	
+	$user_id=$_POST['id'];
+	$user_pw=$_POST['pw'];
+	$user_gender=$_POST['gender'];
+	$user_email=$_POST['email'];
+ 	$school_city=$_POST['city']; 
+	$school_name=$_POST['school'];
+	$user_supervisor=$_POST['supervisor'];
+	
+	$sql = "INSERT INTO `login_info`(`user_id`,`user_pw`,`gender`,`email`,`city`,`school`,`supervisor`) 
+		VALUES ('".$user_id."','".$user_pw."','".$user_gender."','".$user_email."','".$school_city."',
+		'".$school_name."','".$user_supervisor."');";
+	mysql_query($sql, $link) or die('MySQL query error');
+	echo "成功";
+?>
---
+++ register.js
@@ -0,0 +1,40 @@
+
+
+
+$(function(){
+	$("#send").click(function(){
+		if($('#male').is(':checked')){
+			var sex=$("#male").val();
+		}else if($('#female').is(':checked')){
+			var sex=$("#female").val();
+		}else{
+			var sex="";
+		}
+		
+		if($('#user_id').val()!=""&&$('#user_id').val()!=""&&$('#user_pw').val()&&sex!=""&&$("#user_email").val()!=""&&$("#city").val()!=""&&$("#school_name").val()!=""&&$("#teacher_name").val()!=""){
+			$.ajax( {
+				url: 'login_data.php',
+				type: 'POST',
+				data: {
+				id: $('#user_id').val(),	
+				pw: $('#user_pw').val(),
+				gender:sex,
+				email:$("#user_email").val(),
+ 				city:$("#city").val(),
+				school:$("#school_name").val(),
+				supervisor:$("#teacher_name").val()
+				},
+				error: function(xhr) {
+				alert('Ajax request 發生錯誤');
+				},
+				success: function(userid) {
+				alert(userid);
+				}
+			});
+		}else{
+			alert("請輸入資料");
+		}
+	});
+});
+
+
